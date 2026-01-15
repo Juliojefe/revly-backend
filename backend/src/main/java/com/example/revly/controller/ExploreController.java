534ac4e38f6578b97e91fc61d80992e963de017f
Julio Cesar Fernandez <j.c.jf285@gmail.com>
2026-01-15 09:33:54 -0800

    sorting explore posts newest to oldest

--- backend/src/main/java/com/example/revly/controller/ExploreController.java
+++ backend/src/main/java/com/example/revly/controller/ExploreController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,7 +35,7 @@ public class ExploreController {
             Principal principal
     ) {
         User user = getUserFromPrincipalOrThrow(principal);
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
         return ResponseEntity.ok(
                 exploreService.getExplorePosts(pageable, user)
         );
@@ -45,7 +46,7 @@ public class ExploreController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
     ) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
         return ResponseEntity.ok(
                 exploreService.getExplorePostsGuest(pageable)
         );
