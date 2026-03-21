1bea1d9b22c8743435a136dd308f1169359931b2
Juliojefe <j.c.jf285@gmail.com>
2026-03-21 09:02:19 -0700

    working now I need to simplify

--- backend/src/main/java/com/example/revly/RevlyApplication.java
+++ backend/src/main/java/com/example/revly/RevlyApplication.java
@@ -2,8 +2,10 @@ package com.example.revly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RevlyApplication {
 
 	public static void main(String[] args) {
